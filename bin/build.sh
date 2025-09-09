7cdc0b1c689c461e3df7078d1d2e40fa8e5f443e
eth-limo <ethdotlimo@protonmail.com>
2025-09-09 13:48:48 -0400

    Lock dependencies

--- bin/build.sh
+++ bin/build.sh
@@ -2,10 +2,9 @@
 cd bin || exit 1
 packages=$(node ./list_packages.js)
 cd ..
-npm i
+npm ci
 echo "${PWD}"
 for package in ${packages}; do
   echo "Building ${package}"
-  npm i -w "${package}"
   npm run build -w "${package}"
 done
